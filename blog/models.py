405ba3b30786c2fc1157521d7e5be324aaa60039
pokhrelyadav <pokhrelyadav412@gmail.com>
2025-12-17 00:26:43 +0545

    added product model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Product(models.Model):
+    DEVICE_CHOICE = [
+        ('L','Laptop'),
+        ('M','Mobile')
+    ]
+    name = models.CharField(max_length=60)
+    description = models.TextField() #no compulsory max length becaue it for long text
+    image = models.ImageField(upload_to='productImages/')
+    device_type = models.CharField(max_length=2,choices=DEVICE_CHOICE)
+    date_added = models.DateTimeField(default=timezone.now)
