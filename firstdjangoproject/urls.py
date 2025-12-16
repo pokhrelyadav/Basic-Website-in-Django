b21032a0d470ddf735dbe141cdcd7ad5050b8711
pokhrelyadav <pokhrelyadav412@gmail.com>
2025-12-17 00:31:26 +0545

    import django conf and added static in urls.py

--- firstdjangoproject/urls.py
+++ firstdjangoproject/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
@@ -27,4 +30,4 @@ urlpatterns = [
     
     path('__reload__/',include('django_browser_reload.urls'))
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
