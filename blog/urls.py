7a739205e226a072f9340af120b714c8897b46fe
pokhrelyadav <pokhrelyadav412@gmail.com>
2025-12-19 02:38:06 +0545

    created a single product detail page, fetched and displayed correctly

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 #localhost:8000/blog/writers
 urlpatterns = [
     path('',views.get_products,name='blog_home'),
-    path('electronics/', views.get_electronics,name='electronics'),
+    path('electronics/', views.get_electronics,name='blog_electronics'),
+    
+    path('electronics/<int:product_id>/', views.product_detail, name='blog_productdetail')
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Product
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def get_products(req):
@@ -13,3 +14,7 @@ def get_electronics(req):
     # return HttpResponse("Writer: Yadav Pokhrel")
     electronic_product = Product.objects.all()
     return render(req, 'blog/electronics.html', {'allElectronicsList':electronic_product})
+
+def product_detail(req, product_id):
+    oneProduct = get_object_or_404(Product, pk=product_id) # pk or id = same
+    return render(req,'blog/productDetail.html' , {'oneProduct':oneProduct})
