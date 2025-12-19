44b66c7aee0eeb694cd7f27bf5999dd6881ae893
pokhrelyadav <pokhrelyadav412@gmail.com>
2025-12-20 04:33:12 +0545

    added missed trailing comma after path()

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('',views.get_products,name='blog_home'),
     path('electronics/', views.get_electronics,name='blog_electronics'),
     
-    path('electronics/<int:product_id>/', views.product_detail, name='blog_productdetail')
+    path('electronics/<int:product_id>/', views.product_detail, name='blog_productdetail'),
     
 ]
